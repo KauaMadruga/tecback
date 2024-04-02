5b9d346e844478072efe24db969da61adcc88b3f
Kauã Madruga <kakaumadruga.km@gmail.com>
2024-04-02 14:44:12 -0300

    Mudança para o exercício
    alteração no Genero

---
+++ src/main/java/br/com/fujideia/iesp/tecback/model/Genero.java
@@ -0,0 +1,21 @@
+package br.com.fujideia.iesp.tecback.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String genero;
+
+
+
+}
